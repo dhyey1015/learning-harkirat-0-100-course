5ddc36164fc1918813f38b1653e65ba1e2d3768a
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-10-05 13:52:13 +0530

    learned authentication using jwt using an assignment'

--- week3/week3-1/middleware/index.js
+++ week3/week3-1/middleware/index.js
@@ -8,26 +8,26 @@ app.get('/health-checkup', function(req, res){
     const password = req.headers.password;
     const kidneyId = req.query.kidneyId;
     //----------------dumb way to do pre-checks---------------------
-    // //user exist check
-    // if(username != 'xyz1015' && password != 'pass'){
-    //    res.status(403).json({
-    //         msg: "User does not exist"
-    //    }) 
-    //    return;
-    // }
+    //user exist check
+    if(username != 'xyz1015' && password != 'pass'){
+       res.status(403).json({
+            msg: "User does not exist"
+       }) 
+       return;
+    }
 
-    // //kidney input check (1 or 2)
-    // if(kidneyId != 1 && kidneyId != 2){
-    //     res.status(411).json({
-    //         msg: "worng input"
-    //     })
-    //     return;
-    // }
+    //kidney input check (1 or 2)
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(411).json({
+            msg: "worng input"
+        })
+        return;
+    }
 
-    // //do something with kidney here if every check went well
-    // res.json({
-    //     msg: "Your heart is healthy"
-    // });
+    //do something with kidney here if every check went well
+    res.json({
+        msg: "Your heart is healthy"
+    });
 });
 
 app.use((err, req, res, next) =>{
