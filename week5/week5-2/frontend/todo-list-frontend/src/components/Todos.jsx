2372ac0b1822c7d956e9a8ed2a08bf364f0a417a
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-10-19 21:05:42 +0530

    not integrated with backend but static render is done

---
+++ week5/week5-2/frontend/todo-list-frontend/src/components/Todos.jsx
@@ -0,0 +1,11 @@
+
+export function Todos(){
+
+    return(
+        <div>
+            <h1>go to gym</h1>
+            <h2>go to gym from 7-8</h2>
+            <button>Mark as completed</button>
+        </div>
+    )
+}
