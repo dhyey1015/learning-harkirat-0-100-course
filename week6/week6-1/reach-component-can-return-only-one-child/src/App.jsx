46fbe4f297319f7ea647fee6cd4a0c45d51eabc5
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-10-25 20:11:46 +0530

    learned about React.memo to solve the rendering problem

---
+++ week6/week6-1/reach-component-can-return-only-one-child/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import React,{ Fragment } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [title, setTitle] = useState("my name is Dhyey")
+  
+    function updateTitle(){
+      setTitle("my name is " + Math.random())
+    }
+    return (
+      <div>
+          <button onClick={updateTitle}>Update the title</button>
+          <Headers title = {title}></Headers>
+          <Headers title = "Dhyey"></Headers>
+          <Headers title = "Dhyey"></Headers>
+          <Headers title = "Dhyey"></Headers>
+          <Headers title = "Dhyey"></Headers>
+      </div>
+    )
+  }
+
+const Headers = React.memo(function Headers({title}){
+    return (
+      <div>
+        {title}
+      </div>
+    )
+  })
+
+export default App
