e0a391f89fdc2b810871ce0340179a6bf5363778
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-10-26 20:54:43 +0530

    learned about use effect

---
+++ week6/week6-1/learn-useEffect/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos") //https://jsonplaceholder.typicode.com/todos 
+      .then(async (res) => {
+        const data = await res.json()
+        setTodos(data)
+      })
+  },[])
+
+  return (
+    <div>
+    {todos.map(todo => {
+      return (
+      <Todos key={todo.id} title={todo.title} id={todo.id}></Todos>
+      )
+    })}
+   </div>
+  )
+}
+
+function Todos({title, id}){
+  return(
+    <div>
+      <p>-------------------------------------------------------------------------------------------</p>
+      <h1>Task: {id}</h1>
+      <h2>Title: {title}</h2>
+      <p>-------------------------------------------------------------------------------------------</p>
+    </div>
+  )
+}
+
+export default App
