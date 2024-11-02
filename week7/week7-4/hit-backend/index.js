fbf3c8f9860e7b680d70e8f5e1838b4e5e69fe7c
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-03 03:42:39 +0530

    made a backend to hit

---
+++ week7/week7-4/hit-backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const data = [
+    {
+        "network": 7, 
+        "jobs":8,
+        "messaging": 10,
+        "notifications": 5
+    },
+    {
+        "network": 14, 
+        "jobs":20,
+        "messaging": 52,
+        "notifications": 66
+    },
+    {
+        "network": 2, 
+        "jobs":10,
+        "messaging": 16,
+        "notifications": 23
+    },
+    {
+        "network": 45, 
+        "jobs": 82,
+        "messaging": 30,
+        "notifications": 60
+    }
+];
+
+app.get('/data',function(req, res){
+    const randomIndex = Math.floor(Math.random() * data.length);
+    res.json(data[randomIndex]);
+});
+
+
+app.listen(3000);
