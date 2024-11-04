758223e43ff32ff520a7290ac5b257b83605ccc2
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-04 23:48:31 +0530

    learned about storybook and somewhat MUI

---
+++ week8/week8-2/dukkan-task/src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,17 @@
+import { RevenueCard } from "../../components/RevenueCard";
+
+
+const meta = {
+    component: RevenueCard,
+}
+
+export default meta;
+
+export const BigAmount = {
+    args: {
+        title: "Anbjdx",
+        orderCount: true,
+        amount: "92,45,678.12" ,
+        count: 15,
+    }
+}
