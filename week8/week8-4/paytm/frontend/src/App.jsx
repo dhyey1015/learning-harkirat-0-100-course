75ae643db6231fd5e07b48c9e9eb7df57404901a
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-05 17:58:52 +0530

    made pages of sign in and sign up

--- week8/week8-4/paytm/frontend/src/App.jsx
+++ week8/week8-4/paytm/frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+import { Signin } from './pages/Signin'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +13,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/signin" element={<Signin/>}/>
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="send" element={<SendMoney/>} />
+          {/* <Route path="/signup" element={<Signup/>}/> */}
+          <Route path="/" element={<Signin/>}/>
+          {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
+          {/* <Route path="send" element={<SendMoney/>} /> */}
         </Routes>
       </BrowserRouter>
     </>
--- week8/week8-4/paytm/frontend/src/pages/Signup.jsx
+++ week8/week8-4/paytm/frontend/src/pages/Signup.jsx
@@ -1,10 +1,32 @@
 import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import { BottomWarning } from "../components/BottomWarning";
 
 export default function Signup(){
-
     return(
-        <div>
-            <Heading label={"Sign Up"}/>
+        <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                    
+                    <Heading label={"Sign Up"}/>
+                    <SubHeading label={"Enter your infromation to create an account"} />
+                    <div className="border-t-2 border-gray-300 my-4"></div>
+                    <InputBox label={"First Name"} placeholder={"First Name"}/>
+                    <InputBox label={"Last Name"} placeholder={"Last Name"}/>
+                    <InputBox label={"Email"} placeholder={"example@gmail.com"}/>
+                    <InputBox label={"Password"} placeholder={"Password"}/>
+                    <div className="border-t-2 border-gray-300 my-7"></div>
+                    <div className="pt-4">
+                        <Button label={"Sign up"}/>
+                    </div>
+                    <BottomWarning label={"Already have an account?"} buttonText={"Sign in"}/>
+                </div>
+                
+            </div>
+            
         </div>
+        
     )
 }
