dd38eba9f668dc1d29f1ac2920f9e94714048ef7
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-05 22:22:27 +0530

    frontend done

--- week8/week8-4/paytm/frontend/src/App.jsx
+++ week8/week8-4/paytm/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/SendMoney'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/signin" element={<Signin/>}/>
           <Route path="/dashboard" element={<Dashboard/>} />
-          {/* <Route path="send" element={<SendMoney/>} /> */}
+          <Route path="/send" element={<SendMoney/>} />
         </Routes>
       </BrowserRouter>
     </>
