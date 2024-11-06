755738fe20dfea0c6e347b7989cc890be0b37eb1
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-06 22:23:54 +0530

    user part done

--- week8/week8-4/paytm/backend/db.js
+++ week8/week8-4/paytm/backend/db.js
@@ -31,9 +31,22 @@ const UserSchema = new mongoose.Schema({
     },
 })
 
+const accountSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId, //ref to User model
+        ref: 'User',
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true
+    }
+})
+
 const User = mongoose.model("User", UserSchema)
+const Account = mongoose.model("Account", accountSchema)
 
 module.exports = {
     User,
-    
+    Account
 }
