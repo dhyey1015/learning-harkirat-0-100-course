c290c479d8c86f633c106c9107743902abddb3b4
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-07 05:05:07 +0530

    done with video card

--- week8/week8-4/youtube-clone/src/App.jsx
+++ week8/week8-4/youtube-clone/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { VideoCard } from './components/VideoCard'
+import { VideoGrid } from './components/VideoGrid'
+import { Appbar } from './components/Appbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      hi there
+      <div>
+        <Appbar/>
+      </div>
+      <div>
+        <VideoGrid/>
+      </div>
+      
     </div>
   )
 }
