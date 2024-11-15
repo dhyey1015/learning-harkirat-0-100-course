0c24642b16bbc1c4233300d705adc56986eed3ad
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-16 01:51:00 +0530

    basic of ts revise

---
+++ week12-p-1/week12-1/advance-typescript-apis/src/basic-prerequest-should-know.ts
@@ -0,0 +1,21 @@
+interface User{
+    name: string,
+    age: number,
+}
+
+function sumOfAge(user1: User, user2: User){
+    return user1.age + user2.age
+}
+
+const sum = sumOfAge(
+    {
+        name: "dhyey1015",
+        age: 19,
+    },
+    {
+        name: "dhyey1818",
+        age: 20,
+    }
+)
+
+console.log(sum);
