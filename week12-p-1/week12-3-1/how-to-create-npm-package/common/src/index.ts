ab70135ef5ae74fd60600c8ad3e5021156d5d6a8
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-01-01 23:17:34 +0530

    learned about how to make npm packages and how everything works in an npm pakage|| task: complete the todo application

--- week12-p-1/week12-3-1/how-to-create-npm-package/common/src/index.ts
+++ week12-p-1/week12-3-1/how-to-create-npm-package/common/src/index.ts
@@ -3,6 +3,17 @@ import z from 'zod';
 export const signupInput = z.object({
     username: z.string(),
     password: z.string()
-})
+});
 
 export type SignupInput = z.infer<typeof signupInput>;
+
+export function isOdd(num: number){
+    if(num%2 == 0){
+        return true;
+    }
+    return false;
+}
+
+export function firstEl(arr: string[]): string{
+    return arr[0];
+}
