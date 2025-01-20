df221a5bb5e133ea5abec272584ffb53fb2f2f88
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-01-20 19:47:40 +0530

    added all basic routes needed

--- week13/medium/medium-clone/src/index.ts
+++ week13/medium/medium-clone/src/index.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.get('/api/v1/user/signup', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c)=> {
+  return c.text('heelo');
+})
+
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('ehl');
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('ebdcu');
+})
+
+app.get('/api/v1/blog/bulk', (c)=> {
+  return c.text('huduc')
+})
 export default app
