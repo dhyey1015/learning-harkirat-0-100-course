7d63bf50c9c31cf3b3d12e9546c0bd4e3b82cd3f
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-01-30 23:46:46 +0530

    done with blog home page ui (not polished)

---
+++ week13/medium/frontend-medium-clone/src/components/AppBar.tsx
@@ -0,0 +1,26 @@
+
+
+export function AppBar(){
+    return(
+        <div className="border-b border-slate-200 flex justify-between px-10 py-5">
+            <div className="text-4xl font-bold">
+                Medium
+            </div>
+            <div className="flex flex-col justify-center h-10 w-10">
+                <Avatar name= "dhyey"/>
+            </div>
+        </div>
+    )
+}
+
+
+function Avatar({name}: {name: string}){
+    const newName = name.toUpperCase()
+    return(
+        <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden 
+                bg-gray-400 rounded-full text-gray-800 font-normal hover:bg-gray-300 
+                transition-colors duration-200">
+            <span className="text-xl text-white">{newName[0]}</span>
+        </div>
+    )
+}
---
+++ week13/medium/frontend-medium-clone/src/components/BlogCard.tsx
@@ -0,0 +1,50 @@
+
+interface BlogCardInterface {
+    authorName: string;
+    title: string;
+    content: string;
+    publishedDate: string;
+}
+
+export function BlogCard({authorName, title, content, publishedDate} : BlogCardInterface) {
+
+    return(
+        <div className="p-4 border-b border-slate-200">
+           
+            <div className="flex pb-2 ">
+                <div className="">
+                    <Avatar name={authorName} />
+                </div>
+                <div className="flex justify-center flex-col font-normal pl-2 text-sm">
+                    {authorName} 
+                </div>
+                <span className="flex justify-center flex-col pl-1 font-bold text-slate-400">&middot;</span> 
+                <div className="flex justify-center flex-col pl-1 text-slate-600 text-sm font-extralight">
+                    {publishedDate}  
+                </div>
+            </div>
+            <div  className="text-xl font-extrabold pb-1">
+                {title}
+            </div>
+            <div className="text-sm font-normal text-slate-500 pb-2">
+                {content.slice(0,200) + "..."}
+            </div>
+            <div className="text-slate-400 text-xs">
+                {`${Math.ceil(content.length/100)} minute(s) read`}
+            </div>
+            {/* <div className="bg-slate-100 h-0.5 w-full"></div> */}
+        </div>
+
+    )
+}
+
+function Avatar({name}: {name: string}){
+    const newName = name.toUpperCase()
+    return(
+        <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden 
+                bg-gray-400 rounded-full text-gray-800 font-normal hover:bg-gray-300 
+                transition-colors duration-200">
+            <span className="text-white">{newName[0]}</span>
+        </div>
+    )
+}
